015034a7b1f3418f70243324daa0e882ab84bf4e
Tyler Sheft <twsandmds@gmail.com>
2022-03-10 21:16:04 -0800

    Initial Commit

---
+++ SheftAppsStylishUI/SheftAppsStylishUI.h
@@ -0,0 +1,18 @@
+//
+//  SheftAppsStylishUI.h
+//  SheftAppsStylishUI
+//
+//  Created by Tyler Sheft on 3/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SheftAppsStylishUI.
+FOUNDATION_EXPORT double SheftAppsStylishUIVersionNumber;
+
+//! Project version string for SheftAppsStylishUI.
+FOUNDATION_EXPORT const unsigned char SheftAppsStylishUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SheftAppsStylishUI/PublicHeader.h>
+
+
