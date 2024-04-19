276036c7813ffe825c061eb84e8d39ad35d00dbb
TechWithTyler <twsandmds@gmail.com>
2024-04-19 14:00:42 -0700

    Added alignment/spacing parameters to TranslucentFooterVStack

--- SheftAppsStylishUI/SwiftUI/Containers and Scenes/TranslucentFooterVStack.swift
+++ SheftAppsStylishUI/SwiftUI/Containers and Scenes/TranslucentFooterVStack.swift
@@ -18,18 +18,37 @@ public struct TranslucentFooterVStack<MainContent: View, FooterContent: View>: V
     
     let translucentFooterContent: () -> FooterContent
 
-    /// Creates a new `TranslucentFooterVStack` with the given main content and footer content.
-    public init(@ViewBuilder mainContent: @escaping () -> MainContent, @ViewBuilder translucentFooterContent: @escaping () -> FooterContent) {
+    let mainAlignment: HorizontalAlignment
+
+    let footerAlignment: HorizontalAlignment
+
+    let mainSpacing: CGFloat?
+
+    let footerSpacing: CGFloat?
+
+    /// Creates a new `TranslucentFooterVStack` with the given content, alignment, and spacing.
+    /// - Parameters:
+    ///   - mainAlignment: Horizontal alignment of the main content.
+    ///   - mainSpacing: Spacing between items in the main content.
+    ///   - footerAlignment: Horizontal alignment of the translucent footer content.
+    ///   - footerSpacing: Spacing between items in the translucent footer content.
+    ///   - mainContent: The main content of the stack.
+    ///   - translucentFooterContent: The content of the translucent footer.
+    public init(mainAlignment: HorizontalAlignment = .center, mainSpacing: CGFloat? = nil, footerAlignment: HorizontalAlignment = .center, footerSpacing: CGFloat? = nil, @ViewBuilder mainContent: @escaping () -> MainContent, @ViewBuilder translucentFooterContent: @escaping () -> FooterContent) {
         self.mainContent = mainContent
         self.translucentFooterContent = translucentFooterContent
+        self.mainAlignment = mainAlignment
+        self.footerAlignment = footerAlignment
+        self.mainSpacing = mainSpacing
+        self.footerSpacing = footerSpacing
     }
     
     public var body: some View {
-        VStack {
+        VStack(alignment: mainAlignment, spacing: mainSpacing) {
             mainContent()
         }
         .safeAreaInset(edge: .bottom, spacing: 0) {
-            VStack {
+            VStack(alignment: footerAlignment, spacing: footerSpacing) {
                 translucentFooterContent()
             }
             .padding(.vertical, 10)
