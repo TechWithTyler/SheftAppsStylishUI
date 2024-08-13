0b5709ddacda9a2a68ae4ef707613da1f6b96f94
TechWithTyler <twsandmds@gmail.com>
2024-08-13 14:11:36 -0700

    Replaced escape key code with escape key Unicode string

--- SheftAppsStylishUI/Supporting Files/Globals.swift
+++ SheftAppsStylishUI/Supporting Files/Globals.swift
@@ -8,8 +8,21 @@
 
 import Foundation
 
+// MARK: - Properties - Strings
+
 /// A string that can be used to preview font/text settings in SheftApps apps.
 public let SATextSettingsPreviewString: String = "The quick brown fox jumps over the lazy dog."
+
+/// The key equivalent string for the return key.
+public let SAReturnKeyEquivalentString: String = "\r"
+
+/// The key equivalent string for the delete key.
+public let SADeleteKeyEquivalentString: String = "\u{232B}"
+
+/// The key equivalent string for the escape key.
+public let SAEscapeKeyEquivalentString: String = "\u{1B}"
+
+// MARK: - Properties - Doubles
 
 /// The minimum font size for text in SheftApps apps.
 public let SATextViewMinFontSize: Double = 14
@@ -19,6 +32,8 @@ public let SATextViewMaxFontSize: Double = 48
 
 /// The range of font sizes for text in SheftApps apps.
 public let SATextViewFontSizeRange: ClosedRange<Double> = SATextViewMinFontSize...SATextViewMaxFontSize
+
+// MARK: - Properties - Floats
 
 /// The corner radius for text views in SheftApps apps.
 public let SATextViewCornerRadius: CGFloat = 5
@@ -31,13 +46,3 @@ public let SAContainerViewCornerRadius: CGFloat = 5
 
 /// A corner radius value that creates a circle (no corners) used for image views which display a person's photo in SheftApps apps.
 public let SAPersonPhotoViewCornerRadius: CGFloat = 360
-
-/// The key equivalent string for the return key.
-public let SAReturnKeyEquivalentString: String = "\r"
-
-/// The key equivalent string for the delete key.
-public let SADeleteKeyEquivalentString: String = "\u{232B}"
-
-// 16-bit integers work on 64-bit systems.
-/// The key code for the escape key.
-public let SAEscapeKeyCode: UInt16 = 53
