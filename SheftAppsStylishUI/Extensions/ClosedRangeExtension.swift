b56137b4fb5f56a9208470323b6cfb1bac75b826
TechWithTyler <twsandmds@gmail.com>
2025-02-18 13:25:21 -0800

    Update ClosedRangeExtension.swift

--- SheftAppsStylishUI/Extensions/ClosedRangeExtension.swift
+++ SheftAppsStylishUI/Extensions/ClosedRangeExtension.swift
@@ -8,8 +8,18 @@
 
 import Foundation
 
-/// Static properties for getting a specific range of numeric values.
+/// Static methods and properties for getting a specific range of numeric values.
 public extension ClosedRange where Bound: Numeric & Comparable {
+
+    private static var maxValue: Bound {
+        if let intType = Bound.self as? any BinaryInteger.Type {
+            return (intType.init(Int.max) as? Bound)!
+        } else if let floatType = Bound.self as? any BinaryFloatingPoint.Type {
+            return (floatType.init(Float.greatestFiniteMagnitude) as? Bound)!
+        } else {
+            fatalError("Unsupported type: \(type(of: Bound.self))")
+        }
+    }
 
     /// A range consisting of all possible positive numbers, including 0.
     static var allPositivesIncludingZero: ClosedRange<Bound> {
@@ -31,13 +41,4 @@ public extension ClosedRange where Bound: Numeric & Comparable {
         return 1...maxValue
     }
 
-    private static var maxValue: Bound {
-        if let intType = Bound.self as? any BinaryInteger.Type {
-            return (intType.init(Int.max) as? Bound)!
-        } else if let floatType = Bound.self as? any BinaryFloatingPoint.Type {
-            return (floatType.init(Float.greatestFiniteMagnitude) as? Bound)!
-        } else {
-            fatalError("Unsupported type: \(type(of: Bound.self))")
-        }
-    }
 }
